0ab1cc085278294ed6afd40e4c8fd2622a105e2b
sibu <sibuna742@gmail.com>
2025-01-20 20:09:45 -0500

    update some file

--- script5.js
+++ script5.js
@@ -1,22 +1,34 @@
-let fname=document.getElementById("name");
-let pwd=document.getElementById("pswd");
-let p=document.createElement("p");
-let edt=document.getElementById("edit");
-const sbmtbutton=document.getElementById("submit");
+let fname = document.getElementById("name");
+let pwd = document.getElementById("pswd");
+let p = document.createElement("p");
+let edt = document.getElementById("edit");
+var ul = document.getElementById("list");
+const sbmtbutton = document.getElementById("submit");
 
-sbmtbutton.addEventListener("click",function(event){
+sbmtbutton.addEventListener("click", function (event) {
     event.preventDefault();
-    let x=fname.value;
-    // console.log(x)
-    let y=pwd.value;
-// console.log(y)
-    if(x && y){
-   
-       alert("username and password successfully submited")
-       
+    let x = fname.value;
+    let y = pwd.value;
+
+    if (x !== null && y !== null) {
+        alert("username and password submitted successfully");
+        document.getElementById("name").value = " ";
+        document.getElementById("pswd").value = " ";
+    location.reload();
     }
-    else{
-        alert("please enter a valid username or password")
-    }
-   
-})
+});
+// function addItem(){
+// 	var ul = document.getElementById("dynamic-list");
+//   var candidate = document.getElementById("candidate");
+//   var li = document.createElement("li");
+//   li.setAttribute('id',candidate.value);
+//   li.appendChild(document.createTextNode(candidate.value));
+//   ul.appendChild(li);
+// }
+
+// function removeItem(){
+// 	var ul = document.getElementById("dynamic-list");
+//   var candidate = document.getElementById("candidate");
+//   var item = document.getElementById(candidate.value);
+//   ul.removeChild(item);
+// }
